2288a12c32995fed889bd262f3c1ed6dd5862b33
Rudenko Stanislav <rudenkostas2@gmail.com>
2021-10-08 15:42:45 +0300

    adding login router

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,7 @@ const {Router} = require('express')
 const bcrypt = require('bcryptjs')
 const config = require('config')
 const {check, validationResult} = require('express-validator')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 const router = Router()
 
@@ -40,6 +41,58 @@ router.post(
         res.status(500).json({message: "Something is wrong"})
     }
 })
+
+
+// /api/auth/login
+router.post(
+    '/login', 
+    [
+        check('email', 'Email is not correct').isEmail(),
+        check('password', 'Enter the password').exists(),
+        check('password', 'Minimum password length is 6 symbols').isLength({ min: 6 })
+    ],
+    async (req, res)=>{
+    try{
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Email or password is not correct"
+            
+            })
+        }
+
+        const {email, password} = req.body
+
+        const user = await User.findOne({email})
+
+        if(!user){
+            return res.status(400).json("User has not been found")
+        }
+
+        const isMatch = bcrypt.compare(password, user.password)
+
+        if(!iSMatch){
+            return res.status(400).json({message:"Password is not correct, please try again"})
+        }
+
+        const token = jwt.sign(
+            {userId: user.id},
+            config.get('jwtSecret'),
+            {expiresIn : '1h'}
+        )
+
+        res.json({token, userId: user.id})
+        
+        res.status(201).json({message: "User has been created"})
+    } catch(e){
+        res.status(500).json({message: "Something is wrong"})
+    }
+})
+
+
+
 
 
 module.exports = router
