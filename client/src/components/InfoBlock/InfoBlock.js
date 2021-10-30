46fd7c3dc8573df34ae8ee1ed1bf3a5b55fd0c4f
Rudenko Stanislav <rudenkostas2@gmail.com>
2021-10-30 20:39:22 +0300

    adding football field

--- client/src/components/InfoBlock/InfoBlock.js
+++ client/src/components/InfoBlock/InfoBlock.js
@@ -3,8 +3,27 @@ import "./InfoBlock.css"
 const InfoBlock = ()=>{
     return(
         <div className="infoBlock">
-            Info Block
-            <img src="https://s.ill.in.ua/i/football/team/logo/0x160/5.png" />
+            <div className="footballField">
+                <div className="positionsWrapper">
+                        <div className="fieldPostion"></div>
+                        <div className="fieldPostion"></div>
+                </div>
+                <div className="positionsWrapper">
+                        <div className="fieldPostion"></div>
+                        <div className="fieldPostion"></div>
+                        <div className="fieldPostion"></div>
+                        <div className="fieldPostion"></div>
+                </div>
+                <div className="positionsWrapper">
+                        <div className="fieldPostion"></div>
+                        <div className="fieldPostion"></div>
+                        <div className="fieldPostion"></div>
+                        <div className="fieldPostion"></div>
+                </div>
+                <div className="positionsWrapper">
+                        <div className="fieldPostion"></div>
+                </div>
+            </div>
         </div>
     )
 }
